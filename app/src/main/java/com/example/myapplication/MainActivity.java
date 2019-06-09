0b2f8021a73bdbe6eb7eea062632c3c8603efdb1
noborack74 <46844526noborack74@users.noreply.github.com>
2019-06-10 00:44:42 +0900

    added continuous listening

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.Manifest.permission.RECORD_AUDIO;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView myWebView;
-    private String accessUrl = "https://youtube.com/";
+    private TextView textView;
+    private static final int REQUEST_CODE = 12345;
+    private Button button;
+
+    private Intent intent;
+    private SpeechRecognizer mRecognizer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,40 +43,229 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //web view作成
-        myWebView = findViewById(R.id.webview);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
 
-        //setting系
-        WebSettings webSettings = myWebView.getSettings();
-        //javascript　有効化
-        webSettings.setJavaScriptEnabled(true);
-        //web storage を有効化
-        webSettings.setDomStorageEnabled(true);
 
-        //別のブラウザ開かないように
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                //ページ読み込みが完了してからJavascriptが利用可能になる
-                onScriptReady();
+
+        if (ContextCompat.checkSelfPermission(this, RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, RECORD_AUDIO)) {
+                // 拒否した場合
+            } else {
+                // 許可した場合
+                int MY_PERMISSIONS_RECORD_AUDIO = 1;
+                ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO}, MY_PERMISSIONS_RECORD_AUDIO);
             }
-        });
+        }
 
-        myWebView.setWebChromeClient(new WebChromeClient());
 
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.JAPAN.toString());
+        intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
 
-        // url読み込み
-        myWebView.loadUrl(accessUrl);
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(listener);
+
+
 
     }
 
-    private void onScriptReady() {
-        //最初のスクリプト内関数実行
-        myWebView.loadUrl("javascript:setTimeout(() => {var videos = document.getElementsByClassName('large-media-item-thumbnail-container');videos[1].click();"+
-                "},10000);");
+    public void buttonClicked(View view) {
+
+
+
+        restartListeningService();
+        Log.d("button", "録音開始 ");
+
+
+        //mRecognizer.startListening(intent);
+
+
+        /*
+        //音声認識用のインテントを作成
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        //認識する言語を指定（この場合は日本語）
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.JAPANESE.toString());
+        //認識する候補数の指定
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+        //音声認識時に表示する案内を設定
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "話してください");
+
+        //音声認識を開始
+        startActivityForResult(intent, REQUEST_CODE);*/
     }
+
+    //音声認識が終わると自動で呼び出されるメソッド
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+            //data から音声認識の結果を取り出す（リスト形式で）
+            ArrayList<String> kekka = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            //認識結果が一つ以上ある場合はテキストビューに結果を表示する
+            if (kekka.size() > 0) {
+                //一番最初にある認識結果を表示す
+                textView.setText(kekka.get(0));
+            } else {
+                //何らかの原因で音声認識に失敗した場合はエラーメッセージを表示
+
+                button.setText("0!");
+
+            }
+        }
+    }
+
+
+
+
+
+    private RecognitionListener listener = new RecognitionListener() {
+
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            Log.d("log:: ", "準備できてます");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            Log.d("log:: ", "始め！");
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+            Log.d("log:: ", "音声が変わった");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+            Log.d("log:: ", "新しい音声");
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.d("log:: ", "終わりました");
+        }
+
+
+        @Override
+        public void onError(int i) {
+            switch (i) {
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+
+                    textView.setText("ネットワークタイムエラー");
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+
+                    textView.setText("その外ネットワークエラー");
+                    break;
+                case SpeechRecognizer.ERROR_AUDIO:
+
+                    textView.setText("Audio エラー");
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+
+                    textView.setText("サーバーエラー");
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+
+                    textView.setText("クライアントエラー");
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+
+                    textView.setText("何も聞こえてないエラー\nもう一度");
+
+
+
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+
+                    textView.setText("適当な結果を見つけてませんエラー");
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    textView.setText("RecognitionServiceが忙しいエラー");
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    textView.setText("RECORD AUDIOがないエラー");
+                    break;
+            }
+            restartListeningService();
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            String key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = bundle.getStringArrayList(key);
+
+            String[] result = new String[0];
+            if (mResult != null) {
+                result = new String[mResult.size()];
+            }
+            if (mResult != null) {
+                mResult.toArray(result);
+            }
+
+            textView.setText(result[0]);
+            Log.d("a", "onResults: 終わり");
+            restartListeningService();
+
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+        }
+    };
+
+
+    // 音声認識を開始する
+    protected void startListening() {
+        try {
+            if (mRecognizer == null) {
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+                if (!SpeechRecognizer.isRecognitionAvailable(getApplicationContext())) {
+                    Toast.makeText(getApplicationContext(), "音声認識が使えません",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                mRecognizer.setRecognitionListener(listener);
+            }
+
+            mRecognizer.startListening(intent);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "startListening()でエラーが起こりました",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    // 音声認識を終了する
+    protected void stopListening() {
+        if (mRecognizer != null) mRecognizer.destroy();
+        mRecognizer = null;
+    }
+
+    // 音声認識を再開する
+    public void restartListeningService() {
+        stopListening();
+        startListening();
+    }
+
+
+
+
+
+
 
 
 
